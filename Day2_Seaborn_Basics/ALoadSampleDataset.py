167c5f5456a8d1e66c7fd0e498c22513e063b007
Rupika-08 <99046007+Rupika-08@users.noreply.github.com>
2025-02-16 23:25:11 -0500

    Delete Day2_Seaborn_Basics directory

--- Day2_Seaborn_Basics/ALoadSampleDataset.py
+++
@@ -1,7 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-#Seaborn comes with built-in datasets.
-#Load titanic dataset
-titanic = sns.load_dataset('titanic')
-#Display the first few rows
-print(titanic.head())
